af998e203d488e4e0cd4c449231ca674b5c5235a
AYMERIC-PC\admin <xhin_12@hotmail.fr>
2017-06-27 00:01:38 +0200

    Sprites : Sun & Cloud Cloud Animator AudioController

---
+++ Grow/Assets/Scripts/AudioController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioClip[] musicClips;
+
+    AudioSource audioSource;
+    int excludeMusic;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        int randMusic = Random.Range(0, musicClips.Length);
+        if (randMusic == excludeMusic) return;
+        audioSource.clip = musicClips[randMusic];
+        excludeMusic = randMusic;
+        audioSource.Play();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!audioSource.isPlaying)
+        {
+            Start();
+        }
+    }
+}
